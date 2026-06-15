7a20b5f8604f7f33b032d86a031b03a701674cc6
gimontania <gimontania@gmail.com>
2026-06-15 13:35:42 -0300

    primer commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+/**Parte 1: ejercicios prácticos */
+/**Reto 1 */
+let ctaA = 10;
+let ctaB = 5;
+
+/**sumo y lo guardo en ctaA */
+ctaA = ctaA + ctaB;
+/**al valor actual ctaA le resto ctaB */
+ctaB = ctaA - ctaB;
+/**al valor actual ctaA, le resto valor actual ctaB */
+ctaA = ctaA - ctaB;
+
+console.log("cuenta A:", ctaA);
+console.log("cuenta B:", ctaB);
+
+
+
+/**Reto 2 */
